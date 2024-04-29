0fe251f950c6c8d4620fc157172e13d12ca5d949
= <luigi.cfranca@gmail.com>
2024-04-29 12:06:00 -0300

    Refactor renderMenuTableItems to receive each row count

--- src/menu/menu-table-controller.js
+++ src/menu/menu-table-controller.js
@@ -1,3 +1,4 @@
+import { MenuItem } from "./menu-item.js";
 import { MenuTable } from "./menu-table.js";
 import { MenuTableRow } from "./menu-table-row.js";
 
@@ -8,7 +9,7 @@ export class MenuTableController {
   constructor(menuTable) {
     if (menuTable === undefined)
       throw new TypeError(
-        "Missing pararameter in the creation of a new menu table controller. You must give a menu table instance."
+        "Missing pararameter in the creation of a new MenuTableController. You must give a MenuTable instance."
       );
     else if (!(menuTable instanceof MenuTable))
       throw new TypeError(
@@ -18,9 +19,34 @@ export class MenuTableController {
     this.#menuTable = menuTable;
   }
 
-  renderMenuTableItems(tbodyElement, menuItems = this.#menuTable.menuItems) {
+  renderMenuTableItems(
+    tbodyElement,
+    menuItemsCount = new Map(),
+    menuItems = this.#menuTable.menuItems
+  ) {
+    if (tbodyElement === undefined)
+      throw new TypeError(
+        "Missing parameter in the rendering of the MenuTableItems. You must give a tbody element."
+      );
+    else if (!(tbodyElement instanceof HTMLElement))
+      throw new TypeError(
+        "Invalid type for tbody element, this field must be an HTMLElement instance."
+      );
+    else if (!(menuItemsCount instanceof Map))
+      throw new TypeError(
+        "Invalid type for menu items count, this field must be a Map instance."
+      );
+    else if (menuItems.some((item) => !(item instanceof MenuItem)))
+      throw new TypeError(
+        "Invalid type for menu items, all items must be instances of MenuItem."
+      );
+
     menuItems.forEach((menuItem) => {
-      const menuTableRow = new MenuTableRow(menuItem);
+      let menuItemQuantity = 0;
+      if (menuItemsCount.has(menuItem.id))
+        menuItemQuantity = menuItemsCount.get(menuItem.id);
+
+      const menuTableRow = new MenuTableRow(menuItem, menuItemQuantity);
       tbodyElement.appendChild(menuTableRow.htmlElement);
 
       this.#menuTableRows.push(menuTableRow);
