799243e8878bccf6e042fc1f65e0ad1023f02268
= <luigi.cfranca@gmail.com>
2024-05-01 14:14:01 -0300

    Update retrieveUserCart function to retrieve a map with the cart data

--- src/user-cart/retrieve-user-cart.js
+++ src/user-cart/retrieve-user-cart.js
@@ -2,5 +2,12 @@ export function RetrieveUserCart() {
   const cartItemsJSON = localStorage.getItem("userCart");
   if (cartItemsJSON === null) return null;
 
-  return JSON.parse(cartItemsJSON);
+  const userCartData = JSON.parse(cartItemsJSON);
+  const userCart = new Map();
+  userCartData.forEach((cartItemAndQuantity) => {
+    const { cartItem, quantity } = cartItemAndQuantity;
+    userCart.set(cartItem, quantity);
+  });
+
+  return userCart;
 }
