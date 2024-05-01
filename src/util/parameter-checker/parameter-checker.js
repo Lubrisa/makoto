f1e840b30e818df38c482e6139b376fd04b6bb62
= <luigi.cfranca@gmail.com>
2024-05-01 13:43:34 -0300

    Remove parameter validator files

--- src/util/parameter-checker/parameter-checker.js
+++
@@ -1,7 +0,0 @@
-export class ParameterChecker {
-  static checkIfUndefined(...parameters) {
-    if (parameters.some((parameter) => parameter === undefined))
-      throw new TypeError("Missing parameter.");
-    return this;
-  }
-}
